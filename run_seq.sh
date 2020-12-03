2e8899c1e82b8a2fb31451549692d3795123d020
Katinskaia Anisia <anisia.katinskaia@helsinki.fi>
2020-12-03 15:04:06 +0200

    Run script

---
+++ run_seq.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#SBATCH --account=project_2002016
+#SBATCH --job-name=seq_ged
+#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=5
+#SBATCH --mem=15G
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH --gres=gpu:v100:1
+
+module load gcc/8.3.0 cuda/10.1.168
+
+export DIR=/projappl/project_2002016/sequence-labeler
+cd $DIR
+
+conda activate transf
+
+srun python3 experiment.py ./conf/fcepublic.conf
