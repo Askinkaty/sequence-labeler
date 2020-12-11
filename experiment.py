1e441db909e94f9b33b690c19448d3bfbfb924d3
Katinskaia Anisia <anisia.katinskaia@helsinki.fi>
2020-12-11 10:54:07 +0200

    Minor fix

--- experiment.py
+++ experiment.py
@@ -22,7 +22,7 @@ def filter_sentences(words):
     for word in words:
         if len(word):
             word = word.replace('...', '.').replace('..', '.').replace('""""', '"').replace('"""', '"')
-            if '-' in word:  # here we split hyphenated tokens by hyphen
+            if '-' in word and len(word) > 1:  # here we split hyphenated tokens by hyphen
                 filtered.extend([el.strip() for el in word.split('-')])
             else:
                 if len(word):
@@ -49,8 +49,6 @@ def read_input_files(file_paths, max_sentence_length=-1):
                     assert(len(line_parts) == line_length or line_length == None)
                     line_parts = [el for el in line_parts if len(el)]
                     line_length = len(line_parts)
-                    if 'азартного' in line_parts:
-                        print(line_parts)
                     filtered = filter_sentences(line_parts)
                     sentence.append(filtered)
                 elif len(line) == 0 and len(sentence) > 0:
