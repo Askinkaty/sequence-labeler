04236de7f426d29745ba52f0aa1006c2a8d26729
Katinskaia Anisia <anisia.katinskaia@helsinki.fi>
2020-12-13 10:25:46 +0200

    Mods in config

--- run_seq.sh
+++ run_seq.sh
@@ -3,8 +3,8 @@
 #SBATCH --account=project_2002016
 #SBATCH --job-name=seq_ged
 #SBATCH --partition=gpu
-#SBATCH --cpus-per-task=5
-#SBATCH --mem=15G
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=32G
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00
 #SBATCH --gres=gpu:v100:1
@@ -16,4 +16,4 @@ cd $DIR
 
 conda activate transf
 
-srun python3 ./sequence-labeler/experiment.py ./sequence-labeler/conf/fcepublic.conf
+srun python3 ./sequence-labeler/experiment.py ./sequence-labeler/conf/gram_det.conf
