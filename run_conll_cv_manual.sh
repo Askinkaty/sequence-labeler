ed03bf080fa1bf079585876f23e898a9a957eacb
Katinskaia Anisia <anisia.katinskaia@helsinki.fi>
2020-12-29 20:25:37 +0200

    Minor fix

---
+++ run_conll_cv_manual.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#SBATCH --account=project_2002016
+#SBATCH --job-name=conll_cv_manual
+#SBATCH --partition=gpu
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --ntasks=1
+#SBATCH --time=48:00:00
+#SBATCH --gres=gpu:v100:1
+
+module load gcc/8.3.0 cuda/10.1.168
+
+export DIR=/projappl/project_2002016
+cd $DIR
+
+conda activate transf
+
+srun python3 ./sequence-labeler/experiment.py ./sequence-labeler/conf/conll_cv.conf
